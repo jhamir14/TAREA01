96c0e7c68eb84b8ae349fae643704848e595a225
jhamir14 <jhamirquispe2007@gmail.com>
2025-08-18 13:44:14 -0500

    python

---
+++ holamundo.py
@@ -0,0 +1,27 @@
+print ("Hello world")
+
+# tipos de datos
+print ("hola")   #print imprimer y string es para texto
+# entero es integer
+print (2)
+# decimal es floar 
+print (3.21)
+#boolean
+True
+False
+#list es el array 
+[1,2,3,4]
+["hola","bye","chau"]
+[1,"hola",2,"chau"]
+# tuples son datos inmudables
+(10,30,24)
+# dictorionies
+{
+    "name":"jhamir",
+    "lastname":"quispe",
+    "nickname":"amir"
+}
+
+#Variables
+name="jhamir"
+
